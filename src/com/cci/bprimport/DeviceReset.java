1cd524d4de9aec5bd356c7fc476de66e895dfb76
Matt Reath <mpreath@gmail.com>
2012-08-29 14:23:56 -0500

    Updated licensing information

--- src/com/cci/bprimport/DeviceReset.java
+++ src/com/cci/bprimport/DeviceReset.java
@@ -1,3 +1,19 @@
+// DeviceReset - part of bprimport, DeviceReset connects to the Cisco 
+// BAC RDU and sends a DeviceReset command for a particular device.
+// Copyright (C) 2012  Matt Reath
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package com.cci.bprimport;
 
 import com.cisco.provisioning.cpe.*;
